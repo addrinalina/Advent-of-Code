0c5aaf8bcde6f3533155e56e2bbd34e5954c1ca3
addrinalina <adriromo@hotmail.es>
2023-12-03 12:51:03 +0100

    Update aoc3.py

--- aoc3.py
+++ aoc3.py
@@ -42,21 +42,86 @@ def adjacent_symbol(matrix, row, col):
 
     return result
 
+def adjacent_gear(matrix, row, col):
+    result = False
+    coords = []
+    if col - 1 >= 0:
+        if matrix[row][col - 1] == "*":
+            result = True
+            coords = [row,col-1]
+
+    if col + 1 < len(matrix[0]):
+        if matrix[row][col + 1] == "*":
+            result = True
+            coords = [row, col+1]
+
+    if row - 1 >= 0:
+        if matrix[row - 1][col] == "*":
+            result = True
+            coords = [row-1, col]
+
+    if row + 1 < len(matrix):
+        if matrix[row + 1][col] == "*":
+            result = True
+            coords = [row+1, col]
+
+    if row - 1 >= 0 and col - 1 >= 0:
+        if matrix[row - 1][col - 1] == "*":
+            result = True
+            coords = [row-1, col-1]
+
+    if row - 1 >= 0 and col + 1 < len(matrix[0]):
+        if matrix[row - 1][col + 1] == "*":
+            result = True
+            coords = [row-1, col+1]
+
+    if row + 1 < len(matrix) and col - 1 >= 0:
+        if matrix[row + 1][col - 1] == "*":
+            result = True
+            coords = [row+1, col-1]
+
+    if row + 1 < len(matrix) and col + 1 < len(matrix[0]):
+        if matrix[row + 1][col + 1] == "*":
+            result = True
+            coords = [row+1, col+1]
+
+    return result, coords
+
 rows = len(x)
 columns = len(x[0])
 num = ""
 partnumber = False
+gearnumber = False
 partlist = []
+gearlist = []
+gearratios = []
 for i in range(rows):
     for j in range(columns):
         if not x[i][j].isdigit():
             if partnumber:
                 partlist.append(int(num))
-            num = ""
             partnumber = False
+            if gearnumber:
+                gearlist.append([int(num), gearcoords])
+            gearnumber = False
+            num = ""
         else:
             if adjacent_symbol(x, i, j):
                 partnumber = True
             num += x[i][j]
-print(partlist)
-print(sum(partlist))
+            if adjacent_gear(x,i,j)[0]:
+                gearnumber = True
+                gearcoords = adjacent_gear(x,i,j)[1]
+print("List of part numbers", partlist)
+print("Sum of part numbers", sum(partlist))
+
+print("List of gear numbers", gearlist)
+
+for i, number in enumerate(gearlist):
+    for j in range(i+1, len(gearlist)):
+        if number[1] == gearlist[j][1]:
+            print("Gear pair", number[0], gearlist[j][0])
+            gearratios.append(number[0]*gearlist[j][0])
+
+print("List of gear ratios", gearratios)
+print("Sum of gear ratios", sum(gearratios))

0958595cf426c6573df5e2ecd9dc6f226f985da0
addrinalina <100372811+addrinalina@users.noreply.github.com>
2023-12-04 09:58:20 +0100

    aoc4-2

--- aoc4.py
+++ aoc4.py
@@ -1,7 +1,8 @@
+# https://adventofcode.com/2023/day/4
+
 with open("C:\\Users\\aromo\\OneDrive - No Hunger Forum\\Documentos\\Advent of Code\\input4.txt") as f:
     lines = f.readlines()
     x = [line[:-1] for line in lines[:-1]] + [lines[-1]]
-print(x)
 
 points = []
 
@@ -31,11 +32,7 @@ for i, game in enumerate(x):
             num = ""
         if propios and num != "":
             if int(num) in winning:
-                # print("El numero", num, "es ganador del juego", i+1)
-                if calculo == 0:
-                    calculo = 1
-                else:
-                    calculo *= 2
+                calculo += 1
             num = ""
         j += k+1
     # print("Los winning numbers del game", i, "son", winning)
@@ -44,3 +41,18 @@ for i, game in enumerate(x):
 
 print("Los juegos ganan estos puntos:",points)
 print("La suma de los puntos es: ", sum(points))
+
+copies = [1 for _ in range(len(x))]
+
+for i in range(len(points)):
+    cards = copies[i]
+    while cards > 0:
+        j = i+1
+        number = points[i]
+        while number > 0:
+            copies[j] += 1
+            number -= 1
+            j += 1
+        cards -= 1
+print("Copias: ",copies)
+print("La suma de las copias es", sum(copies))

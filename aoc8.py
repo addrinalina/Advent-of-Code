fa94533c065a7edb96b8a05efb25487635f348d7
addrinalina <adriromo@hotmail.es>
2023-12-09 17:29:13 +0100

    aoc8-2

--- aoc8.py
+++ aoc8.py
@@ -1,11 +1,16 @@
 # https://adventofcode.com/2023/day/8
 
 import ast
+import math
+from functools import reduce
 
 with open("C:\\Users\\adrir\\Documents\\GitHub\\Advent-of-Code-23\\input8.txt") as f:
     lines = f.readlines()
     x = [line[:-1] for line in lines[:-1]] + [lines[-1]]
 print(x)
+
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
 
 nodes = {}
 rightleft = []
@@ -29,15 +34,27 @@ print(rightleft)
 print(nodes)
 
 i = 0
-loc = 'AAA'
 
-while loc != 'ZZZ':
+print(nodes.keys())
+
+loc = [node for node in nodes.keys() if node.endswith('A')]
+
+print(loc)
+cicles = [0]*len(loc)
+
+while 0 in cicles:
     j = i
     if j >= len(rightleft):
         j = j%len(rightleft)
-    newloc = nodes[loc][rightleft[j]]
-    loc = newloc
-    i += 1
-    print(loc)
+    for k, node in enumerate(loc):
+        newloc = nodes[node][rightleft[j]]
+        loc[k] = newloc
+        if newloc.endswith('Z') and cicles[k] == 0:
+            cicles[k] = i+1
+    i += 1    
 
-print("Total steps", i)
+print(cicles)
+
+mcm = reduce(lcm, cicles)
+
+print("Steps = ", mcm)
